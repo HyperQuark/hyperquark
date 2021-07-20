44761c56b45b32e1ebb00b4042805c14018cd16e
Glitch (hyperquark) <none>
2021-07-20 08:11:47 +0000

    🏘☺️ Checkpoint ./vite.config.js:17134896/293

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
+const path = require("path");
+
 export default {
   css: {
     modules: {
       localsConvention: 'camelCaseOnly'
     }
-  }
+  },
+  build: {
+    outDir: "build"
+  },
+  server: {
+    strictPort: true,
+    hmr: {
+      port: 443 // Run the websocket server on the SSL port
+    }
+  },
+  resolve: {
+    alias: {
+      find: '@/',
+      replacement: path.resolve(__dirname, './src')
+    }
+  },
 }
