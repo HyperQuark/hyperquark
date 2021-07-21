04177e2f60d06340ac4891b1350f9d24a4d7aab0
Glitch (hyperquark) <none>
2021-07-21 15:03:01 +0000

    ☃️🚐 Checkpoint ./test.as:17134896/4 ./rollup-as-loader.js:17134896/117

--- rollup-as-loader.js
+++ rollup-as-loader.js
@@ -15,14 +15,12 @@ export function assemblyScript() {
       for (let option in compilerOptions) {
         compilerOptions[option] ?? (compilerOptions[option] = true)
       }
-      console.log(fileId, query, compilerOptions);
       let z = await new Promise(async (resolve, reject) => {
         await asc.ready;
         const { binary, text } = asc.compileString(
           readFileSync(fileId, { encoding: "utf-8" }),
           compilerOptions
         );
-        writeFileSync("/app/aa.md", text, { encoding: "utf-8" });
         const moo = "import { instantiate as asInstantiate} from \"@assemblyscript/loader\";\
         export const instantiate = options => new Promise(async resolve => resolve(await asInstantiate(new Uint8Array([" + binary.toString() + "]), options)));\
           export const text = '" + text.replace(/\\/g, "\\\\").replace(/'/g, "\\'").replace(/\n/g, '\\n') + "';";
--- test.as
+++ test.as
@@ -1,14 +1,11 @@
-export * from '../node_modules/asdom/assembly/glue'
+export * from './node_modules/asdom/assembly/glue'
 
-import {document} from '../node_modules/asdom/assembly/index'
+import {document} from './node_modules/asdom/assembly/index'
 
 export function start (): void {
   const el = document.createElement('h1')
-
   el.setAttribute('foo', 'bar')
-
   const s: string = el.getAttribute('foo')! // returns "bar"
-
   el.innerHTML = /*html*/ `
     <span style="font-weight: normal; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)">
       <em>hello</em> from <strong>AssemblyScript</strong>
