7be4b469338bf07dec782ee5ff07279d1ce110b5
Glitch (hyperquark) <none>
2021-07-21 08:56:20 +0000

    🌲😘 Checkpoint ./rollup-as-loader.js:17134896/108

--- rollup-as-loader.js
+++ rollup-as-loader.js
@@ -9,11 +9,11 @@ export function assemblyScript () {
       return new Promise(async (resolve, reject) => {
         await asc.ready;
         const { binary, text, stdout, stderr } = asc.compileString(code, asc.options);
-        resolve({
-          code: `
+        const moo = `
           export const instantiate = options => new Promise(async resolve => resolve(await WebAssembly.instantiate(new Uint8Array([${binary.toString()}]), options)));
-          `
-        });;
+          `;
+        console.log(moo);
+        resolve({code: moo});
       });
     }
   }
