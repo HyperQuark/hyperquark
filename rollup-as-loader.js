22a99171552879dcd62892b887134ff5ba147ae5
Glitch (hyperquark) <none>
2021-07-21 11:04:13 +0000

    💡🐚 Checkpoint ./rollup-as-loader.js:17134896/1710

--- rollup-as-loader.js
+++ rollup-as-loader.js
@@ -1,21 +1,26 @@
-const asc = require("assemblyscript/cli/asc");
+import asc from "assemblyscript/cli/asc";
+import { readFileSync } from "fs";
 
-export function assemblyScript () {
+export function assemblyScript() {
   return {
     name: "assemblyscript",
-    async transform (code, id) {
+    async load(id) {
       console.log("aaaaaa");
       if (!/\.(t|a)s$/.test(id)) return;
+      let 
       let z = await new Promise(async (resolve, reject) => {
         await asc.ready;
-        const { binary, text } = asc.compileString(code);
+        const { binary, text } = asc.compileString(
+          readFileSync(fileId, { encoding: "utf-8" }),
+          compilerOptions
+        );
         const moo = `
           export const instantiate = options => new Promise(async resolve => resolve(await WebAssembly.instantiate(new Uint8Array([${binary.toString()}]), options)));
           `;
         console.log(moo);
-        resolve({code: moo});
+        resolve({ code: moo });
       });
       return z;
     }
-  }
+  };
 }
