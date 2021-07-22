78b5dd9498d3ea9fb070fc0b4d82360cb7ca2448
Glitch (hyperquark) <none>
2021-07-22 12:47:54 +0000

    🎻👱 Checkpoint ./test.ts:17134896/6

--- built.ts
+++ built.ts
@@ -1,4 +1,4 @@
-// @ts-expect-error
+/*// @ts-expect-error
 @external('asDOM', 'trackNextRef')
 export declare function trackNextRef(id: usize): void
 // @ts-expect-error
@@ -798,13 +798,13 @@ export function start(): void {
   const el = document.createElement("h1");
   el.setAttribute("foo", "bar");
   const s: string = el.getAttribute("foo")!; // returns "bar"
-  el.innerHTML = /*html*/ `
+  el.innerHTML = /*html*/ /*`
     <span style="font-weight: normal; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)">
       <em>hello</em> from <strong>AssemblyScript</strong>
     </span>
   `;
   document.body!.appendChild(el);
-}
+}*/
 export function add(a: i32, b: i32): i32 {
   return a + b;
 }
