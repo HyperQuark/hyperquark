fae56a669512d7f5b975e167ec51b51c642be10e
Glitch (hyperquark) <none>
2021-07-23 09:46:51 +0000

    🙆🏂 Checkpoint ./rollup-as-loader.js:17134896/1362

--- rollup-as-loader.js
+++ rollup-as-loader.js
@@ -1,4 +1,4 @@
-import asc from "assemblyscript/cli/asc";
+import { ready as ascReady, createMemoryStream, main as ascMain } from "assemblyscript/cli/asc";
 import { readFileSync, writeFileSync } from "fs";
 import { resolve as resolvePath } from "path";
 import { parse as parseQueryString } from "query-string";
@@ -7,7 +7,7 @@ function resolveImport(id, cache) {
   let code = readFileSync(id, { encoding: "utf-8" });
 //  code = code.replace(/\/\*.??\*\//gms, "");
 //  code = code.replace(/\/\/.+$/gm, "");
-  code = code.replace(
+  /*code = code.replace(
     /(?:(?:import|export) +.+?from +(?:"|'))(.+?)(?:(?:"|');?$)/gms,
     (m, p) => {
       if (/^\.\./.test(p)) p = "../" + p;
@@ -22,35 +22,38 @@ function resolveImport(id, cache) {
         return resolveImport(absId, cache) + "\n";
       }
     }
-  );
+  );*/
   //code = code.replace(/\/\*.+?\*\//gms, "");
   return code.replace(/\s*?\n+\s*?/gm, "\n")
     // handle weird error edge cases
-    .replace("export class ShadowRootInit", "class ShadowRootInit")
-    .replace("export declare function cloneNode", "declare function cloneNode")
-  .replace("ERROR('NodeList is not writable.')", "")
+  //  .replace("export class ShadowRootInit", "class ShadowRootInit")
+   // .replace("export declare function cloneNode", "declare function cloneNode")
+ // .replace("ERROR('NodeList is not writable.')", "")
 }
 
 async function load(id) {
   let cache = {};
-  console.log("aaaaaa");
+ // console.log("aaaaaa");
   if (!/\.(t|a)s(\?.*?)?$/.test(id)) return;
   let [fileId, query] = id.split("?");
   let compilerOptions = parseQueryString(query || "", {
-    //  parseNumbers: true
+    parseNumbers: true
   });
   for (let option in compilerOptions) {
     compilerOptions[option] ?? (compilerOptions[option] = true);
   }
   let z = await new Promise(async (resolve, reject) => {
-    await asc.ready;
+    await ascReady;
     // let code = readFileSync(fileId, { encoding: "utf-8" });
     let code = resolveImport(fileId, cache);
-    writeFileSync("/app/built.ts", code, {encoding:"utf-8"});
+   // writeFileSync("/app/built.ts", code, {encoding:"utf-8"});
    // console.log(code);
-    console.log(/@ts-expect-error/.test(code));
+  //  console.log(/@ts-expect-error/.test(code));
     
-      var { binary, text, stderr } = asc.compileString(code, compilerOptions);
+      var { binary, text, stderr } = compileString(code, {
+        ...compilerOptions,
+        baseDir: "/app/"
+      });
     
       if (stderr.length) console.error(stderr.toString());
     
@@ -75,3 +78,35 @@ export const assemblyScript = () => ({
   name: "assemblyscript",
   load
 });
+
+const compileString = (sources, options) => {
+  if (typeof sources === "string") sources = { "input.ts": sources };
+  const output = Object.create({
+    stdout: createMemoryStream(),
+    stderr: createMemoryStream()
+  });
+  var argv = [
+    "--binaryFile", "binary",
+    "--textFile", "text",
+  ];
+  Object.keys(options || {}).forEach(key => {
+    var val = options[key];
+    var opt = exports.options[key];
+    if (opt && opt.type === "b") {
+      if (val) argv.push("--" + key);
+    } else {
+      if (Array.isArray(val)) {
+        val.forEach(val => { argv.push("--" + key, String(val)); });
+      }
+      else argv.push("--" + key, String(val));
+    }
+  });
+  exports.main(argv.concat(Object.keys(sources)), {
+    stdout: output.stdout,
+    stderr: output.stderr,
+    readFile: name => readFileSync("/app/" + name, { encoding: "utf-8" }),
+    writeFile: (name, contents) => { output[name] = contents; },
+    listFiles: () => []
+  });
+  return output;
+};
