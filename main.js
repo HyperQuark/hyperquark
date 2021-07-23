b88cace9c9c81207ffa26318ca2fd2aec407dc78
Glitch (hyperquark) <none>
2021-07-23 12:42:04 +0000

    👕💄 Checkpoint ./main.js:17134896/482 ./public/sw.js:17134896/888

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ import "./style.css";
 import { instantiate, text } from "./test.ts?exportTable&exportRuntime&explicitStart";
 import { Asdom } from "asdom/glue/index.js";
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+}
+
 const asdom = new Asdom();
 
 instantiate({ env: { abort: () => console.log("Abort!") }, ...asdom.wasmImports }).then(
---
+++ public/sw.js
@@ -0,0 +1,31 @@
+var CACHE_NAME = 'hyperquark-cache-v1';
+var urlsToCache = [
+  
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheAllowlist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
