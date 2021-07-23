0dbf541833d2ed0ccec0dea720df16397ebb78ea
Glitch (hyperquark) <none>
2021-07-23 14:26:53 +0000

    ⚽️🚙 Checkpoint ./public/sw.js:17134896/3549

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@ self.addEventListener("install", function(event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
-     // console.log("Opened cache");
+      // console.log("Opened cache");
       return cache.addAll(urlsToCache);
     })
   );
@@ -34,10 +34,11 @@ self.addEventListener("fetch", async event => {
 function customHeaderRequestFetch(event) {
   return new Promise((resolve, reject) => {
     fetch(event.request).then(response => {
-      console.log(new Map(response.headers));
+    //  console.log(mapToObj(new Map(response.headers)));
 
       const newHeaders = new Headers(response.headers);
-      newHeaders.append('x-foo', 'bar');
+      newHeaders.append("Cross-Origin-Opener-Policy", "same-origin");
+      newHeaders.append("Cross-Origin-Embedder-Policy", "require-corp");
 
       const anotherResponse = new Response(response.body, {
         status: response.status,
@@ -45,8 +46,14 @@ function customHeaderRequestFetch(event) {
         headers: newHeaders
       });
 
-      console.log(new Map(anotherResponse.headers));
+    //  console.log(mapToObj(new Map(anotherResponse.headers)));
       resolve(anotherResponse);
     });
   });
+}
+
+function mapToObj(map) {
+  const obj = {};
+  for (let [k, v] of map) obj[k] = v;
+  return JSON.stringify(obj);
 }
