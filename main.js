6117c63a916d466fb0aecc515377ef4a086e7392
Glitch (hyperquark) <none>
2021-08-28 16:35:40 +0000

    ⛲️✍️ Checkpoint ./main.js:17134896/13728

--- main.js
+++ main.js
@@ -33,14 +33,14 @@ if ("serviceWorker" in navigator) {
     }
   );
 } else document.body.textContent = "You're using an unsupported browser :(";
-
-async function main() {
-  const memory = new WebAssembly.Memory({
+*/
+function main() {
+  /* const memory = new WebAssembly.Memory({
     shared: true,
     initial: 11,
     maximum: 100
-  });
-  
+  });*/
+
   document.getElementById("app").innerHTML = `
     <button id="start">green flag</button>
     <button id="stop">stop</button>
@@ -48,45 +48,77 @@ async function main() {
     <br>
     <br>
     Running: <span id="running">false</span>
-  `
-  let { exports } = await instantiate({});
+  `;
+  // let { exports } = await instantiate({});
   //console.log(wasm.exports.table.get(wasm.exports.e())());
-  window.wasm = exports;
-  console.log(wasm.e(), wasm.b());
-  console.log(wasm.table.get(wasm.e())(54455445));
-  console.log(wasm.table.get(wasm.b())());
- /* let vm = new VM({ memory });
+  // window.wasm = exports;
+  //console.log(wasm.e(), wasm.b());
+  //console.log(wasm.table.get(wasm.e())(54455445));
+  // console.log(wasm.table.get(wasm.b())());
+  /* let vm = new VM({ memory });
   await vm.init();
   let renderer = new Renderer({ canvas: document.getElementById("stage"), memory });
   renderer.start();
 */
-//}
-const wasmHeader = [0, 97, 115, 109, 1, 0, 0, 0];
-const encodeSignedLeb128FromInt32 = (value) => {
-  value |= 0;
-  const result = [];
-  while (true) {
-    const byte = value & 0x7f;
-    value >>= 7;
-    if (
-      (value === 0 && (byte & 0x40) === 0) ||
-      (value === -1 && (byte & 0x40) !== 0)
-    ) {
-      result.push(byte);
-      return result;
+  //}
+  const wasmHeader = [0, 97, 115, 109, 1, 0, 0, 0];
+  const encodeSignedLeb128FromInt32 = value => {
+    value |= 0;
+    const result = [];
+    while (true) {
+      const byte = value & 0x7f;
+      value >>= 7;
+      if (
+        (value === 0 && (byte & 0x40) === 0) ||
+        (value === -1 && (byte & 0x40) !== 0)
+      ) {
+        result.push(byte);
+        return result;
+      }
+      result.push(byte | 0x80);
     }
-    result.push(byte | 0x80);
+  };
+  const createSection = (type, content) => [
+    type,
+    encodeSignedLeb128FromInt32(content.length),
+    ...content
+  ];
+  const typeSection = types => createSection(0x01, types);
+  const types = {
+    i32: 0x7f,
+    i64: 0x7e,
+    f32: 0x7d,
+    f64: 0x7c
+  };
+  const funcType = (paramTypes = [], returnTypes = []) => {
+    let e = [
+    0x60,
+    paramTypes.length,
+    ...paramTypes,
+      returnTypes.length,
+      ...returnTypes
+    ];
+    console.log(paramTypes, returnTypes, e)
+    return e;
   }
-};
-const createSection = (type, content) => [type, encodeSignedLeb128FromInt32(content.length), ...content];
-const typeSection = (types) => createSection(0x01, types);
-const types = {
-  i32: 0x7F,
-  i64: 0x7E,
-  f32: 0x7D,
-  f64: 0x7C
+  // we shouldn't needimports, here just in case
+  // const importSection = imports => createSection(2, imports);
+  const createWasmModule = ({ types }) => {
+    //return new Uint8Array(
+     return wasmHeader.concat(
+        typeSection(types.map(t => funcType(t.params, t.returns)))
+      )
+    //);
+  };
+  console.log(
+    createWasmModule({
+      types: [
+        {
+          params: [types.i32, types.i32],
+          returns: []
+        }
+      ]
+    })
+  );
 }
-const funcType = (paramTypes = [], returnTypes = []) => [0x60, paramTypes.length, ...paramTypes, returnTypes.length, ...returnTypes];
-// we shouldn't needimports, here just in case
-// const importSection = imports => createSection(2, imports);
-const createWasmModule = ({ types }) => new Uint8Array(wasmHeader.concat(types.map(t => typeSection(funcType(t.params, t.returns)))));
+main();
