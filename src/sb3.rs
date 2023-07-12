c11811a9c063ebc6a55a40b29e3c8ef6fee28def
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2023-07-12 17:04:28 +0100

    lint again?!

--- src/sb3.rs
+++ src/sb3.rs
@@ -337,7 +337,7 @@ impl BlockMap for BTreeMap<String, Block> {
     fn get_bottom_block(&self, id: &str) -> &Block {
         let block = self.get(id).unwrap();
         if let Some(next) = &block.block_info().unwrap().next {
-            self.get_bottom_block(&next)
+            self.get_bottom_block(next)
         } else {
             block
         }
