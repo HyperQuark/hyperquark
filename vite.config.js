d4619ac8465a8e0b380f83cdc40358b3326fe1b1
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2023-07-19 10:44:48 +0100

    update vite base

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './playground',
+  base: '/hyperquark/',
   plugins: [
     wasm(),
     vue(),
