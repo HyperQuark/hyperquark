da53c47f3e443aef4d354a879ae64abb41c076e7
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2023-07-26 14:47:18 +0100

    enum-field-getter: support getters for tuple variants

--- enum-field-getter/src/lib.rs
+++ enum-field-getter/src/lib.rs
@@ -13,7 +13,9 @@ pub fn enum_field_getter(stream: TokenStream) -> TokenStream {
         let variants = enum_data.variants.iter();
         let name = info.ident;
         let mut field_info: HashMap<String, (Type, Vec<String>)> = HashMap::new();
+        let mut tuple_field_info: HashMap<usize, (Type, Vec<String>)> = HashMap::new();
         let mut incompatible = HashSet::<String>::new();
+        let mut tuple_incompatible = HashSet::<usize>::new();
         for variant in variants {
             if let Fields::Named(_) = variant.fields {
                 for field in &variant.fields {
@@ -31,25 +33,26 @@ pub fn enum_field_getter(stream: TokenStream) -> TokenStream {
                     }).or_insert(df);
                 }
             } else if let Fields::Unnamed(_) = variant.fields {
-                emit_warning!(variant, "getters are not emitted for tuple variants");
-                /*for field in &variant.fields {
-                    let ident = field.ident.clone().unwrap().to_string();
+                for (i, field) in variant.fields.iter().enumerate() {
                     let field_ty = field.ty.clone();
                     let df = (field_ty.clone(), vec![variant.ident.to_string()]);
-                    field_info.entry(ident.clone()).and_modify(|info| {
+                    tuple_field_info.entry(i).and_modify(|info| {
                         let (ty, used_variants) = info;
                         if quote!{#field_ty}.to_string() != quote!{#ty}.to_string() {
                             emit_warning!(field, "fields must be the same type across all variants - no getter will be emitted for this field");
-                            incompatible.insert(ident.clone());
+                            tuple_incompatible.insert(i);
                         } else {
                             used_variants.push(variant.ident.to_string());
                         }
                     }).or_insert(df);
-                }*/
+                }
             }
         }
         for removeable in incompatible {
             field_info.remove(&removeable);
+        }
+        for tuple_removeable in tuple_incompatible {
+            tuple_field_info.remove(&tuple_removeable);
         }
         let getters = field_info.keys().map(|k| format_ident!("{}", k));
         let types = field_info.values().map(|v| v.0.clone());
@@ -70,11 +73,37 @@ pub fn enum_field_getter(stream: TokenStream) -> TokenStream {
                 }
             }
         });
+        let tuple_getters = tuple_field_info.keys().map(|k| format_ident!("get_{}", k));
+        let tuple_types = tuple_field_info.values().map(|v| v.0.clone());
+        let tuple_field_info_vec = tuple_field_info.iter().collect::<Vec<_>>();
+        let tuple_matches = tuple_field_info_vec.iter().map(|(k, v)| {
+            let variants =
+                v.1.clone()
+                    .iter()
+                    .map(|v| format_ident!("{}", v))
+                    .collect::<Vec<_>>();
+            let preceding = vec![format_ident!("_"); **k];
+            let preceding_quote = vec![quote! { #(#preceding,)* }; variants.len()];
+            let field = vec![format_ident!("val_{}", k); variants.len()];
+            quote! {
+                match self {
+                    #(
+                        Self::#variants(#preceding_quote #field, .. ) => Some(#field),
+                    )*
+                    _ => None,
+                }
+            }
+        });
         quote! {
             impl #name {
                 #(
                 pub fn #getters (&self) -> Option<&#types> {
                     #matches
+                }
+                )*
+                #(
+                pub fn #tuple_getters (&self) -> Option<&#tuple_types> {
+                    #tuple_matches
                 }
                 )*
             }
