9ccb404ff9c4770ab7eade935c6531fcf85efffb
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2023-08-29 16:15:58 +0100

    centre navbar, don't get all spaced out on wide screens

--- playground/App.vue
+++ playground/App.vue
@@ -57,31 +57,4 @@ nav a:first-of-type {
 div.wrapper {
   margin: 0 auto;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    /*margin: 0 2rem 0 0;*/
-  }
-
-  nav .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
