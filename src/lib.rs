4092a1cd9c469daaeef8dd97442d1bd7cb3e0525
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2024-01-19 13:03:57 +0000

    start transforming panics to results

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,17 @@ extern crate enum_field_getter;
 
 use wasm_bindgen::prelude::*;
 
+#[macro_use]
+mod error;
 pub mod ir;
 pub mod sb3;
 pub mod targets;
 
+pub use error::{HQError, HQErrorType};
+
 use targets::wasm;
 
 #[wasm_bindgen]
-pub fn sb3_to_wasm(proj: &str) -> wasm::WasmProject {
-    ir::IrProject::from(sb3::Sb3Project::try_from(proj).expect("uh oh")).into()
+pub fn sb3_to_wasm(proj: &str) -> Result<wasm::WasmProject, HQError> {
+    ir::IrProject::from(sb3::Sb3Project::try_from(proj)?).try_into()
 }
