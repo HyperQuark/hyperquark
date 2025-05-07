2d388f500bd08609909ba952b11b8a2d0f21baa2
pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2025-05-07 10:22:38 +0100

    add 404 page

---
+++ playground/views/404.vue
@@ -0,0 +1,12 @@
+<template>
+    <header><h1>404 Not Found</h1></header>
+    <main>
+        <p>This page doesn't exist.</p>
+        <p><RouterLink to="/">Go home</RouterLink></p>
+    </main>
+</template>
+  
+<script setup>
+    import { RouterLink } from 'vue-router';
+</script>
+  
