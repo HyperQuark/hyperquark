1b1cf82fdc58e6ef41ebec624256ce675a705e26
pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2025-09-25 11:53:27 +0100

    update some styling on the homepage

--- playground/views/HomeView.vue
+++ playground/views/HomeView.vue
@@ -7,9 +7,12 @@ import ProjectInput from '../components/ProjectInput.vue'
     <h1>HyperQuark</h1>
   </header>
   <main>
-    <div>HyperQuark compiles Scratch projects to WebAssembly in order to run them <span id="blazing">⚡ blazingly fast ⚡</span> really quickly.</div>
-    <div><RouterLink to="/about">Learn more</RouterLink></div>
-    <ProjectInput/>
+    <div>HyperQuark compiles Scratch projects to WebAssembly in order to run them <span id="blazing">⚡ blazingly fast
+        ⚡</span> really quickly.</div>
+    <div>
+      <RouterLink to="/about">Learn more</RouterLink>
+    </div>
+    <ProjectInput />
     <br>
     <br>
     <br>
@@ -22,6 +25,7 @@ import ProjectInput from '../components/ProjectInput.vue'
     </ul>
     <br>
     <h2>Currently implemented blocks:</h2>
+    <br>
     <ul id="implemented-opcodes-container">
       <li v-for="opcode in implementedOpcodes" :class="'block-category-' + opcode.match(/^[a-z]+(?=_)/)">
         {{ opcode }}
@@ -31,70 +35,87 @@ import ProjectInput from '../components/ProjectInput.vue'
 </template>
 
 <script>
-  import { opcodes as implementedOpcodes } from '@/../js/opcodes.js';
-  // todo: just use ids, dynamically fetch/cache title, author thumbnail etc
-  // or use a studio like turbowarp
-  // but at the moment this is kind of just a quick-access bar for frequently tested projects
-  const featuredProjects = [
-    {
-      id: 951679565,
-      name: 'Colourful Smiley Face Simulator',
-      author: 'pufferfish_test',
-    },
-    {
-      id: 886414106,
-      name: 'benchmark',
-      author: 'pufferfish_test',
-    },
-    {
-      id: 1050568991,
-      name: 'fibonacci benchmark',
-      author: 'pufferfish_test',
-    },
-    {
-      id: 1116438829,
-      name: 'HQ test project 2.0',
-      author: 'pufferfish_test'
-    }
-  ];
+import { opcodes as implementedOpcodes } from '@/../js/opcodes.js';
+// todo: just use ids, dynamically fetch/cache title, author thumbnail etc
+// or use a studio like turbowarp
+// but at the moment this is kind of just a quick-access bar for frequently tested projects
+const featuredProjects = [
+  {
+    id: 951679565,
+    name: 'Colourful Smiley Face Simulator',
+    author: 'pufferfish_test',
+  },
+  {
+    id: 886414106,
+    name: 'benchmark',
+    author: 'pufferfish_test',
+  },
+  {
+    id: 1050568991,
+    name: 'fibonacci benchmark',
+    author: 'pufferfish_test',
+  },
+  {
+    id: 1116438829,
+    name: 'HQ test project 2.0',
+    author: 'pufferfish_test'
+  }
+];
 </script>
 
 <style scoped>
-  h1 {
-    font-size: 2rem;
-    font-weight: 500;
-    margin-bottom: 1rem;
+h1 {
+  font-size: 2rem;
+  font-weight: 500;
+  margin-bottom: 1rem;
+}
+
+span#blazing {
+  font-style: italic;
+  text-decoration: line-through;
+}
+
+ul {
+  list-style: none;
+  padding: inherit;
+
+  &#implemented-opcodes-container {
+    column-gap: 1.5em;
+    column-width: 25ch;
+    column-count: 4;
   }
-  span#blazing {
-    font-style: italic;
-    text-decoration: line-through;
-  }
-  ul {
-    list-style: none;
-    padding: inherit;
-  }
-  li.block-category-looks {
-    color:#9966FF;
-  }
-  li.block-category-operator {
-    color: #59C059;
-  }
-  li.block-category-motion {
-    color: #4C97FF;
-  }
-  li.block-category-control {
-    color: #FFAB19;
-  }
-  li.block-category-pen {
-    color: #0fBD8C;
-  }
-  li.block-category-data {
-    color: #FF8C1A;
-  }
-  li.block-category-sensing {
-    color: #5CB1D6;
-  }
-  li.block-category-procedures, li.block-category-argument {
-    color: #FF6680;
-  }
+}
+
+li.block-category-looks {
+  color: #9966FF;
+}
+
+li.block-category-operator {
+  color: #59C059;
+}
+
+li.block-category-motion {
+  color: #4C97FF;
+}
+
+li.block-category-control {
+  color: #FFAB19;
+}
+
+li.block-category-pen {
+  color: #0fBD8C;
+}
+
+li.block-category-data {
+  color: #FF8C1A;
+}
+
+li.block-category-sensing {
+  color: #5CB1D6;
+}
+
+li.block-category-procedures,
+li.block-category-argument {
+  color: #FF6680;
+}
 </style>
