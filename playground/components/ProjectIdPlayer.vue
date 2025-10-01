c8583f1142ab63f17cbf30d555ac2a493b3804e2
pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2025-10-01 12:06:15 +0100

    add additional info to project player

--- playground/components/ProjectIdPlayer.vue
+++ playground/components/ProjectIdPlayer.vue
@@ -1,5 +1,5 @@
 <template>
-  <ProjectPlayer v-if="success === true" :json="json" :author="author" :title="title" :zip="null"></ProjectPlayer>
+  <ProjectPlayer v-if="success === true" :json="json" :id="props.id" :author="author" :title="title" :instructions="instructions" :description="description" :zip="null"></ProjectPlayer>
   <template v-else>
     <h1>Project not found</h1>
     This could be because it doesn't exist, or it may be private.
@@ -20,10 +20,14 @@
   const json = ref("");
   const title = ref("");
   const author = ref("");
+  const instructions = ref("");
+  const description = ref("");
   try {
     const apiRes = await (await fetch(`https://trampoline.turbowarp.org/api/projects/${props.id}/`)).json()//.project_token;
     title.value = apiRes.title;
     author.value = apiRes.author.username;
+    instructions.value = apiRes.instructions;
+    description.value = apiRes.description;
     json.value = await fetch(`https://projects.scratch.mit.edu/${props.id}/?token=${apiRes.project_token}`).then(res => {
       if (!res.ok) {
         throw new Error("response was not OK");
