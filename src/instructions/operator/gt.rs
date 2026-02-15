5e9dc8ddeefe4da00d1f31d7f279d824a9399891
pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2026-02-15 19:38:24 +0000

    lt/gt: try to convert strings to numbers first

--- src/instructions/operator/gt.rs
+++ src/instructions/operator/gt.rs
@@ -28,7 +28,10 @@ pub fn wasm(func: &StepFunc, inputs: Rc<[IrType]>) -> HQResult<Vec<InternalInstr
                 End,
             ]
         } else if IrType::String.contains(t2) {
-            // TODO: try converting string to number first (if applicable)
+            let string2float = func.registries().external_functions().register(
+                ("cast", "string2float".into()),
+                (vec![ValType::EXTERNREF], vec![ValType::F64]),
+            )?;
             let int2string = func.registries().external_functions().register(
                 ("cast", "int2string".into()),
                 (vec![ValType::I32], vec![ValType::EXTERNREF]),
@@ -41,11 +44,26 @@ pub fn wasm(func: &StepFunc, inputs: Rc<[IrType]>) -> HQResult<Vec<InternalInstr
                 ),
             )?;
             let extern_local = func.local(ValType::EXTERNREF)?;
+            let float_local = func.local(ValType::F64)?;
+            let block_type = func
+                .registries()
+                .types()
+                .function(vec![ValType::I32], vec![ValType::I32])?;
             wasm![
-                LocalSet(extern_local),
+                LocalTee(extern_local),
+                Call(string2float),
+                LocalTee(float_local),
+                LocalGet(float_local),
+                F64Ne, // test if float_local is NaN
+                If(BlockType::FunctionType(block_type)),
                 Call(int2string),
                 LocalGet(extern_local),
-                Call(string_gt)
+                Call(string_gt),
+                Else,
+                F64ConvertI32S,
+                LocalGet(float_local),
+                F64Gt,
+                End,
             ]
         } else {
             hq_bug!("bad input")
@@ -89,6 +107,10 @@ pub fn wasm(func: &StepFunc, inputs: Rc<[IrType]>) -> HQResult<Vec<InternalInstr
                 End,
             ]
         } else if IrType::String.contains(t2) {
+            let string2float = func.registries().external_functions().register(
+                ("cast", "string2float".into()),
+                (vec![ValType::EXTERNREF], vec![ValType::F64]),
+            )?;
             let float2string = func.registries().external_functions().register(
                 ("cast", "float2string".into()),
                 (vec![ValType::F64], vec![ValType::EXTERNREF]),
@@ -101,18 +123,41 @@ pub fn wasm(func: &StepFunc, inputs: Rc<[IrType]>) -> HQResult<Vec<InternalInstr
                 ),
             )?;
             let extern_local = func.local(ValType::EXTERNREF)?;
+            let float1_local = func.local(ValType::F64)?;
+            let float2_local = func.local(ValType::F64)?;
+            let block_type = func
+                .registries()
+                .types()
+                .function(vec![ValType::F64], vec![ValType::I32])?;
             wasm![
                 LocalSet(extern_local),
+                LocalTee(float1_local),
+                LocalGet(extern_local),
+                Call(string2float),
+                LocalTee(float2_local),
+                LocalGet(float2_local),
+                F64Ne, // test if float_local is NaN
+                LocalGet(float1_local),
+                @isnan(t1),
+                I32Or,
+                If(BlockType::FunctionType(block_type)),
                 Call(float2string),
                 LocalGet(extern_local),
-                Call(string_gt)
+                Call(string_gt),
+                Else,
+                LocalGet(float2_local),
+                F64Gt,
+                End,
             ]
         } else {
             hq_bug!("bad input")
         }
     } else if IrType::String.contains(t1) {
         if IrType::QuasiInt.contains(t2) {
-            // TODO: try converting string to number first
+            let string2float = func.registries().external_functions().register(
+                ("cast", "string2float".into()),
+                (vec![ValType::EXTERNREF], vec![ValType::F64]),
+            )?;
             let int2string = func.registries().external_functions().register(
                 ("cast", "int2string".into()),
                 (vec![ValType::I32], vec![ValType::EXTERNREF]),
@@ -124,9 +169,33 @@ pub fn wasm(func: &StepFunc, inputs: Rc<[IrType]>) -> HQResult<Vec<InternalInstr
                     vec![ValType::I32],
                 ),
             )?;
-            wasm![Call(int2string), Call(string_gt)]
+            let extern_local = func.local(ValType::EXTERNREF)?;
+            let float_local = func.local(ValType::F64)?;
+            let int_local = func.local(ValType::I32)?;
+            wasm![
+                LocalSet(int_local),
+                LocalTee(extern_local),
+                Call(string2float),
+                LocalTee(float_local),
+                LocalGet(float_local),
+                F64Ne, // test if float_local is NaN
+                If(BlockType::Result(ValType::I32)),
+                LocalGet(extern_local),
+                LocalGet(int_local),
+                Call(int2string),
+                Call(string_gt),
+                Else,
+                LocalGet(float_local),
+                LocalGet(int_local),
+                F64ConvertI32S,
+                F64Gt,
+                End,
+            ]
         } else if IrType::Float.contains(t2) {
-            // TODO: try converting string to number first
+            let string2float = func.registries().external_functions().register(
+                ("cast", "string2float".into()),
+                (vec![ValType::EXTERNREF], vec![ValType::F64]),
+            )?;
             let float2string = func.registries().external_functions().register(
                 ("cast", "float2string".into()),
                 (vec![ValType::F64], vec![ValType::EXTERNREF]),
@@ -138,8 +207,35 @@ pub fn wasm(func: &StepFunc, inputs: Rc<[IrType]>) -> HQResult<Vec<InternalInstr
                     vec![ValType::I32],
                 ),
             )?;
-            wasm![Call(float2string), Call(string_gt)]
+            let extern_local = func.local(ValType::EXTERNREF)?;
+            let float2_local = func.local(ValType::F64)?;
+            let float1_local = func.local(ValType::F64)?;
+            wasm![
+                LocalSet(float2_local),
+                LocalTee(extern_local),
+                Call(string2float),
+                LocalTee(float1_local),
+                LocalGet(float1_local),
+                F64Ne, // test if float_local is NaN
+                LocalGet(float2_local),
+                @isnan(t2),
+                I32Or,
+                If(BlockType::Result(ValType::I32)),
+                LocalGet(extern_local),
+                LocalGet(float2_local),
+                Call(float2string),
+                Call(string_gt),
+                Else,
+                LocalGet(float1_local),
+                LocalGet(float2_local),
+                F64Gt,
+                End,
+            ]
         } else if IrType::String.contains(t2) {
+            let string2float = func.registries().external_functions().register(
+                ("cast", "string2float".into()),
+                (vec![ValType::EXTERNREF], vec![ValType::F64]),
+            )?;
             let string_gt = func.registries().external_functions().register(
                 ("operator", "gt_string".into()),
                 (
@@ -147,7 +243,33 @@ pub fn wasm(func: &StepFunc, inputs: Rc<[IrType]>) -> HQResult<Vec<InternalInstr
                     vec![ValType::I32],
                 ),
             )?;
-            wasm![Call(string_gt)]
+            let extern1_local = func.local(ValType::EXTERNREF)?;
+            let extern2_local = func.local(ValType::EXTERNREF)?;
+            let float1_local = func.local(ValType::F64)?;
+            let float2_local = func.local(ValType::F64)?;
+            wasm![
+                LocalTee(extern2_local),
+                Call(string2float),
+                LocalSet(float2_local),
+                LocalTee(extern1_local),
+                Call(string2float),
+                LocalTee(float1_local),
+                LocalGet(float1_local),
+                F64Ne,
+                LocalGet(float2_local),
+                LocalGet(float2_local),
+                F64Ne,
+                I32Or,
+                If(BlockType::Result(ValType::I32)),
+                LocalGet(extern1_local),
+                LocalGet(extern2_local),
+                Call(string_gt),
+                Else,
+                LocalGet(float1_local),
+                LocalGet(float2_local),
+                F64Gt,
+                End,
+            ]
         } else {
             hq_bug!("bad input")
         }
