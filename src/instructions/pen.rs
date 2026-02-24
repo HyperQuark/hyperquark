5cdff6f0acc1d6d5f7fc13d2a86877c0e259450b
pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2026-02-24 22:36:09 +0000

    format

--- src/instructions/pen.rs
+++ src/instructions/pen.rs
@@ -1,7 +1,7 @@
+pub mod changecolorparamby;
 pub mod clear;
 pub mod pendown;
 pub mod penup;
 pub mod setpencolorparamto;
 pub mod setpencolortocolor;
 pub mod setpensizeto;
-pub mod changecolorparamby;
