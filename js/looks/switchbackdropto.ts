08cdd6e611f85e2c087336954509b8485fe68e9a
pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2026-03-26 15:35:38 +0000

    don't try to switch to out of bounds costume

--- js/looks/switchbackdropto.ts
+++ js/looks/switchbackdropto.ts
@@ -4,5 +4,6 @@ export function switchbackdropto(costume_num: number) {
   console.log("switch backdrop to", costume_num);
   console.log(stageIndex(), costumes());
   const costume = costumes()[stageIndex()][costume_num];
+  if (typeof costume === "undefined") return;
   renderer().getSkin(target_skins()[stageIndex()][0]).setSVG(costume.data);
 }
--- js/looks/switchcostumeto.ts
+++ js/looks/switchcostumeto.ts
@@ -2,5 +2,6 @@ import { renderer, costumes, target_skins } from "../shared";
 
 export function switchcostumeto(costume_num: number, target_index: number) {
   const costume = costumes()[target_index][costume_num];
+  if (typeof costume === "undefined") return;
   renderer().getSkin(target_skins()[target_index][0]).setSVG(costume.data);
 }
