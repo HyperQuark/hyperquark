32c274d99b13422876ec877635201eeaf9117047
pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2026-07-28 14:37:51 +0100

    format

--- playground/lib/compile-worker.js
+++ playground/lib/compile-worker.js
@@ -19,44 +19,46 @@ addEventListener("message", ({ data }) => {
       break;
     }
     case "optimise": {
-      import("binaryen").then((imports) => {
-        const binaryen = imports.default;
-        const binaryenModule = binaryen.readBinary(data.wasmBytes);
-        console.log(binaryenModule.emitBinary().length);
-        binaryenModule.setFeatures(
-          // We can't set to Features.All because that enables custom descriptors which causes
-          // some passes to emit exact types, which are probably not actually available in the browser.
-          // TODO: base this on actual available features.
-          binaryen.Features.BulkMemory |
-            binaryen.Features.ExtendedConst |
-            binaryen.Features.GC |
-            binaryen.Features.Multivalue |
-            binaryen.Features.MutableGlobals |
-            binaryen.Features.ReferenceTypes |
-            binaryen.Features.SignExt |
-            binaryen.Features.NontrappingFPToInt |
-            binaryen.Features.Strings |
-            binaryen.Features.TailCall,
-        );
-        binaryen.setOptimizeLevel(3);
-        binaryen.setShrinkLevel(0);
-        binaryenModule.runPasses(["generate-global-effects"]);
-        console.log(binaryenModule.emitBinary().length);
-        binaryenModule.optimize();
-        console.log(binaryenModule.emitBinary().length);
-        binaryenModule.optimize();
-        const wasmBytes = binaryenModule.emitBinary();
-        console.log(wasmBytes.length);
-        postMessage(
-          {
-            wasmBytes,
-          },
-          [wasmBytes.buffer],
-        );
-        console.log("finished optimising");
-      }).catch(e => {
-        throw e;
-      });
+      import("binaryen")
+        .then((imports) => {
+          const binaryen = imports.default;
+          const binaryenModule = binaryen.readBinary(data.wasmBytes);
+          console.log(binaryenModule.emitBinary().length);
+          binaryenModule.setFeatures(
+            // We can't set to Features.All because that enables custom descriptors which causes
+            // some passes to emit exact types, which are probably not actually available in the browser.
+            // TODO: base this on actual available features.
+            binaryen.Features.BulkMemory |
+              binaryen.Features.ExtendedConst |
+              binaryen.Features.GC |
+              binaryen.Features.Multivalue |
+              binaryen.Features.MutableGlobals |
+              binaryen.Features.ReferenceTypes |
+              binaryen.Features.SignExt |
+              binaryen.Features.NontrappingFPToInt |
+              binaryen.Features.Strings |
+              binaryen.Features.TailCall,
+          );
+          binaryen.setOptimizeLevel(3);
+          binaryen.setShrinkLevel(0);
+          binaryenModule.runPasses(["generate-global-effects"]);
+          console.log(binaryenModule.emitBinary().length);
+          binaryenModule.optimize();
+          console.log(binaryenModule.emitBinary().length);
+          binaryenModule.optimize();
+          const wasmBytes = binaryenModule.emitBinary();
+          console.log(wasmBytes.length);
+          postMessage(
+            {
+              wasmBytes,
+            },
+            [wasmBytes.buffer],
+          );
+          console.log("finished optimising");
+        })
+        .catch((e) => {
+          throw e;
+        });
     }
   }
 });
